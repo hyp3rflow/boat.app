6709a22357b4bc60dd62eac718de185e7b05c7df
hyperflow <hyperflow@kakao.com>
2021-08-19 00:11:40 +0900

    init: Init react-native project

---
+++ boat/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App: () => React.ReactNode = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text>boat.app</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
